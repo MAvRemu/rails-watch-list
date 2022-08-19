948c5a27dde8ca6fa710ef5caf1c16e1e5d59f38
Marius van Remundt <marius_v_r@hotmail.com>
2022-08-19 11:47:26 +0200

    aligned images

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,11 @@ url = "https://tmdb.lewagon.com/movie/top_rated"
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)
 
+List.create(name: 'triller')
+List.create(name: 'Action')
+List.create(name: 'Humor')
+List.create(name: 'Top 10')
+
 movies['results'].each do |movie|
   Movie.create(title: movie['original_title'], overview: movie['original_title'], poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}", rating: movie['vote_average'])
 end
